b621d17dcd4fea62fe93b9a24ef78809650f7fec
Martin Jaime Flores Jr. <martin.flores98@tutanota.com>
2026-02-18 16:21:06 +0000

    [#18] Add build hook

---
+++ irods_consortium_continuous_integration_build_hook.py
@@ -0,0 +1,93 @@
+import glob
+import multiprocessing
+import optparse
+import os
+import tempfile
+
+import irods_python_ci_utilities
+
+def install_building_dependencies(externals_directory):
+    externals_list = [
+        'irods-externals-boost1.81.0-2',
+        'irods-externals-nanodbc2.13.0-3',
+        'irods-externals-clang16.0.6-0'
+    ]
+
+    if externals_directory == 'None' or externals_directory is None:
+        irods_python_ci_utilities.install_irods_core_dev_repository()
+        irods_python_ci_utilities.install_os_packages(externals_list)
+    else:
+        package_suffix = irods_python_ci_utilities.get_package_suffix()
+        os_specific_directory = irods_python_ci_utilities.append_os_specific_directory(externals_directory)
+        externals = []
+        for irods_externals in externals_list:
+            externals.append(glob.glob(os.path.join(os_specific_directory, f'{irods_externals}*.{package_suffix}'))[0])
+        irods_python_ci_utilities.install_os_packages_from_files(externals)
+    install_os_specific_dependencies()
+
+def install_os_specific_dependencies_apt():
+    irods_python_ci_utilities.install_os_packages(['cmake', 'make', 'gcc'])
+
+def install_os_specific_dependencies_yum():
+    irods_python_ci_utilities.install_os_packages(['cmake', 'make', 'gcc', 'curl-devel', 'openssl-devel'])
+
+def install_os_specific_dependencies():
+    dispatch_map = {
+        'Ubuntu': install_os_specific_dependencies_apt,
+        'Debian gnu_linux': install_os_specific_dependencies_apt,
+        'Almalinux': install_os_specific_dependencies_yum,
+        'Rocky linux': install_os_specific_dependencies_yum,
+    }
+    try:
+        return dispatch_map[irods_python_ci_utilities.get_distribution()]()
+    except KeyError:
+        irods_python_ci_utilities.raise_not_implemented_for_distribution()
+
+def copy_output_packages(build_directory, output_root_directory):
+    irods_python_ci_utilities.gather_files_satisfying_predicate(
+        build_directory,
+        irods_python_ci_utilities.append_os_specific_directory(output_root_directory),
+        lambda s:s.endswith(irods_python_ci_utilities.get_package_suffix()))
+
+def main(build_directory, output_root_directory, irods_packages_root_directory, externals_directory, debug_build, enable_asan, enable_ubsan, build_test_plugin):
+    install_building_dependencies(externals_directory)
+    if irods_packages_root_directory:
+        irods_python_ci_utilities.install_irods_dev_and_runtime_packages(irods_packages_root_directory)
+    build_directory = os.path.abspath(build_directory or tempfile.mkdtemp(prefix='irods_policy_composition_plugin_build_directory'))
+    additional_cmake_args = []
+
+    if debug_build:
+        additional_cmake_args.extend(['-D', 'BUILD_TESTING_POLICY=ON'])
+    if enable_asan:
+        additional_cmake_args.extend(['-D', 'IRODS_ENABLE_ADDRESS_SANITIZER=ON'])
+    if enable_ubsan:
+        additional_cmake_args.extend(['-D', 'IRODS_ENABLE_UNDEFINED_BEHAVIOR_SANITIZER=ON'])
+    if build_test_plugin:
+        additional_cmake_args.extend(['-D', 'CMAKE_BUILD_TYPE=Debug'])
+
+    irods_python_ci_utilities.subprocess_get_output(['cmake', *additional_cmake_args, os.path.dirname(os.path.realpath(__file__))], check_rc=True, cwd=build_directory)
+    irods_python_ci_utilities.subprocess_get_output(['make', '-j', str(multiprocessing.cpu_count()), 'package'], check_rc=True, cwd=build_directory)
+    if output_root_directory:
+        copy_output_packages(build_directory, output_root_directory)
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('--build_directory')
+    parser.add_option('--output_root_directory')
+    parser.add_option('--irods_packages_root_directory')
+    parser.add_option('--externals_packages_directory')
+    parser.add_option('--debug_build', action='store_true')
+    parser.add_option('--enable_address_sanitizer', dest='enable_asan', action='store_true')
+    parser.add_option('--enable_undefined_behavior_sanitizer', dest='enable_ubsan', action='store_true')
+    parser.add_option('--build_test_plugin', action='store_true')
+    options, _ = parser.parse_args()
+
+    main(options.build_directory,
+         options.output_root_directory,
+         options.irods_packages_root_directory,
+         options.externals_packages_directory,
+         options.debug_build,
+         options.enable_asan,
+         options.enable_ubsan,
+         options.build_test_plugin)
+
